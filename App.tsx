bf743b1e972f1350e9c98793f3848f29876656e5
Vinicius de Moraes <168435228+vinibmoraes@users.noreply.github.com>
2025-05-25 15:37:54 -0300

    Estruturação de pastas de navegação.

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider as PaperProvider } from "react-native-paper";
-
-import AppNavigator from "./src/navigation";
+import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
-    </PaperProvider>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
